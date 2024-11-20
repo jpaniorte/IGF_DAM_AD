5525ac718972ee275c99aa380684e62de3837b2d
José Pérez Aniorte <jose.perezaniorte@europcar.com>
2024-11-20 09:30:28 +0100

    UD 2.3 CSV

---
+++ UD_2/3_CSV/ejercicios.md
@@ -0,0 +1,51 @@
+# Ejercicios UD2 - CSV
+
+## Preparación del ejercicio
+
+Estos ejercicios se realizan sobre el mismo repositorio de los ejercicios "UD 2 - Clase Storage". Simplemente hay que comprobar antes de comenzar:
+
+- Cierra la Pull Request que tengas abierta.
+- Comprueba que tienes todos los tests en verde
+
+
+## Ejercicio 1 (3p)
+
+1. Crea una batería de test feature con el nombre `CsvTest`. Haz uso del comando `php artisan test` para crearlo.
+
+2. Implementa los tests para los métodos index, store, update, delete. **No uses ChatGPT**, los tests de estos 4 métodos son idénticos a los vistos en los ficheros `JsonTest.php` y `HelloWorldControllerTest.php`.
+
+3. Añade a ese fichero el siguiente test correspondiente al método show:
+
+```php
+public function test_show_returns_file_content()
+{
+    Storage::fake('local');
+
+    Storage::put('app/existingfile.csv', 'header1,header2\nvalue1,value2');
+
+    $response = $this->get('/api/csv/existingfile.csv');
+
+    $response->assertStatus(200)
+                ->assertJson([
+                    'mensaje' => 'Fichero leído con éxito',
+                    'contenido' => [
+                        ['header1' => 'value1', 'header2' => 'value2']
+                    ]
+                ]);
+}
+```
+
+
+## Ejercicio 2 (7p)
+
+1. Copia el fichero `materiales/CsvController.php` en la carpeta `app/Http/Controllers`.
+
+2. Implementa los métodos index, store, show, update y destroy de acuerdo con la especificación de los comentarios hasta que todos los test sean superados:
+
+```bash
+php artisan test
+```
+
+# Entrega
+
+Abre una Pull Request a la rama main y, a través del aula virtual, entrega la URL de dicha PL. La corrección será automática y la nota será proporcional a la cantidad de tests superados, por lo que es recomendable comprobar que la salida del job test de la PL supera todo los test.
---
+++ UD_2/3_CSV/materiales/CsvController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\JsonResponse;
+
+class CsvController extends Controller
+{
+    /**
+     * Lista todos los ficheros CSV de la carpeta storage/app.
+     *
+     * @return JsonResponse La respuesta en formato JSON.
+     *
+     * El JSON devuelto debe tener las siguientes claves:
+     * - mensaje: Un mensaje indicando el resultado de la operación.
+     * - contenido: Un array con los nombres de los ficheros.
+     */
+    public function index()
+    {
+        //TODO
+    }
+
+   /**
+     * Recibe por parámetro el nombre de fichero y el contenido CSV y crea un nuevo fichero con ese nombre y contenido en storage/app. 
+     * Devuelve un JSON con el resultado de la operación.
+     * Si el fichero ya existe, devuelve un 409.
+     *
+     * @param filename Parámetro con el nombre del fichero. Devuelve 422 si no hay parámetro.
+     * @param content Contenido del fichero. Devuelve 422 si no hay parámetro.
+     * @return JsonResponse La respuesta en formato JSON.
+     *
+     * El JSON devuelto debe tener las siguientes claves:
+     * - mensaje: Un mensaje indicando el resultado de la operación.
+     */
+    public function store(Request $request)
+    {
+        //TODO
+    }
+
+    /**
+     * Recibe por parámetro el nombre de un fichero CSV el nombre de fichero y devuelve un JSON con su contenido.
+     * Si el fichero no existe devuelve un 404.
+     * Hay que hacer uso lo visto en la presentación CSV to JSON.
+     *
+     * @param name Parámetro con el nombre del fichero CSV.
+     * @return JsonResponse La respuesta en formato JSON.
+     *
+     * El JSON devuelto debe tener las siguientes claves:
+     * - mensaje: Un mensaje indicando el resultado de la operación.
+     * - contenido: El contenido del fichero si se ha leído con éxito.
+     */
+    public function show(string $id)
+    {
+        //TODO
+    }
+
+   /**
+     * Recibe por parámetro el nombre de fichero, el contenido CSV y actualiza el fichero CSV. 
+     * Devuelve un JSON con el resultado de la operación.
+     * Si el fichero no existe devuelve un 404.
+     * Si el contenido no es un JSON válido, devuelve un 415.
+     * 
+     * @param filename Parámetro con el nombre del fichero. Devuelve 422 si no hay parámetro.
+     * @param content Contenido del fichero. Devuelve 422 si no hay parámetro.
+     * @return JsonResponse La respuesta en formato JSON.
+     *
+     * El JSON devuelto debe tener las siguientes claves:
+     * - mensaje: Un mensaje indicando el resultado de la operación.
+     */
+    public function update(Request $request, string $id)
+    {
+        //TODO
+    }
+
+     /**
+     * Recibe por parámetro el nombre de ficher y lo elimina.
+     * Si el fichero no existe devuelve un 404.
+     *
+     * @param filename Parámetro con el nombre del fichero. Devuelve 422 si no hay parámetro.
+     * @return JsonResponse La respuesta en formato JSON.
+     *
+     * El JSON devuelto debe tener las siguientes claves:
+     * - mensaje: Un mensaje indicando el resultado de la operación.
+     */
+    public function destroy(string $id)
+    {
+        //TODO
+    }
+}
